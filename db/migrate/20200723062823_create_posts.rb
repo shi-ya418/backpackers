1cdb93c422bd3839fef3f322d919fb6cdf43c1cf
shi-ya418 <takuya.shiiya@gmail.com>
2020-07-24 14:44:02 +0900

    update

--- db/migrate/20200723062823_create_posts.rb
+++ db/migrate/20200723062823_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.
+      t.text :text
+      t.text :image
+      t.text :video
       t.timestamps
     end
   end
