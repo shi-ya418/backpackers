984d48c1a208b4ccfb4140b58e42d8aae822fc06
shi-ya418 <67771398+shi-ya418@users.noreply.github.com>
2020-07-30 14:53:08 +0900

    Revert "update"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,6 @@ class PostsController < ApplicationController
   
   def create
     Post.create(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
-    end
   end
 
   def destroy
@@ -36,17 +30,11 @@ class PostsController < ApplicationController
   def show
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
-    @like = Like.new
   end
   
-
-  def search
-    @posts = Post.search(params[:keyword])
-  end 
- 
   private
   def post_params
-    params.require(:post).permit(:image, :text, :video, :tag_ids, :good_ids).merge(user_id: current_user.id)
+    params.require(:post).permit(:image, :text, :video, :tag_ids).merge(user_id: current_user.id)
   end
 
   def set_post
@@ -58,5 +46,3 @@ class PostsController < ApplicationController
   end
   
 end
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, uniqueness: {case_sensitive: true}
-  
-  has_many :posts, dependent: :destroy
+  has_many :posts
   has_many :comments
-  has_many :likes, dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
+  has_many :goods
 
-  def already_liked?(post)
-    self.likes.exists?(post_id: post.id)
-  end
+  validates :username, presence: true, uniqueness: {case_sensitive: true}
 end
