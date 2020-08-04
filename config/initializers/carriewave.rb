ac70121831106741494e9ac52c103409b99d2355
shi-ya418 <takuya.shiiya@gmail.com>
2020-08-04 11:23:17 +0900

    update

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -3,10 +3,10 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:AKIAVG3PPIMBXGNGRWOU],
-      aws_secret_access_key: Rails.application.credentials.aws[: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],
-      region: 'ap-northeast-1'
+      :provider               => 'AWS',
+      :aws_access_key_id      => 'AKIAVG3PPIMBXGNGRWOU',
+      :aws_secret_access_key  => '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',
+      :region                 => 'ap-northeast-1'
     }
     config.fog_directory  = 'backpackers418'
   end
