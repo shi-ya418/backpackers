82d298ec675d78586482483fc75ef27d0c388430
shi-ya418 <takuya.shiiya@gmail.com>
2020-08-27 19:03:04 +0900

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @posts = current_user.posts
+    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
     unless @user.id == current_user.id
